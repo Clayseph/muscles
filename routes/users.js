fcaaf12ddffd72980537d85b6acf8b094c546d64
Clayton <clayton.yarborough@gmail.com>
2018-09-24 20:23:16 -0400

    adds users with password object to take care of login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  username: String,
+  password: String,
+});
+
+let Users = mongoose.model('Users', userSchema);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Users.find((err, users)=>{
+    res.json(users)
+  });
 });
+
+router.route('/login')
+  .post((req, res) => {
+  Users.findOne({
+    username: req.body.username,
+    password: req.body.password
+  }).then((user,err) => {
+    if(err){
+      res.send(err)
+    }else{
+      res.send(user)
+    }
+  });
+});
+
+router.route('/add')
+  .post((req, res)=>{
+    let user = new Users(req.body);
+    user.save(err=>{
+      if(err){
+        res.send(err)
+      }else{
+        res.send("User Saved")
+      }
+    });
+  });
+
+router.route('/remove')
+.delete((req,res)=>{
+  console.log('romeovos')
+    const query = {'_id': req.body._id}
+    Users.findOneAndRemove(
+        query,
+        req.body,
+        (err)=>{
+            if (err) {
+                return res.status(500);
+            }
+            return res.send("Successfully Removed");
+        }
+        )
+      });
 
 module.exports = router;
